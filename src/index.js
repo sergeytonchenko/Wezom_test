2e47cc82af2f849765a4ff0aed931e2150b6d4ca
sergeytonchenko <randick56@gmail.com>
2020-07-24 17:04:21 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/style.css';
 import './scss/main.scss';
 
+
+const singIn = document.querySelector(".nav__button");
+singIn.addEventListener("click" , () => alert('Спасибо!'))
+
+
+
+
+const callMe = document.querySelector(".header__button");
+callMe.addEventListener("click" , () => alert('Позвони мне, позвони'))
+
+
+// const btnTab = document.querySelectorAll('.filterContent__tabs-btn');
+// for (let i = 0; i < btnTab.length; i++) {
+//     btnTab[i].onclick = function () {
+//         btnTab[i].classList.add('active');    
+//     }   
+    
+// }
+
+
+    
+  
+

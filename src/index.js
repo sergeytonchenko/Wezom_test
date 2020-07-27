4eb6c5cc9a109f698005f6e1757df35ed1208865
sergeytonchenko <randick56@gmail.com>
2020-07-27 11:59:04 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -6,22 +6,28 @@ const singIn = document.querySelector(".nav__button");
 const popup1 = document.querySelector(".popup1");
 const close = document.querySelector(".popup1__enter-close");
 const wrapperBg = document.querySelector(".wrapper__bg");
-singIn.addEventListener("click" , function () {
+
+singIn.addEventListener("click" , () => {
     popup1.style.display = "flex";
     wrapperBg.style.display = "block";
 })
 
-close.addEventListener("click" , function () {
+close.addEventListener("click" , () => {
     popup1.style.display = "none";
     wrapperBg.style.display = "none";    
 })
 
-
-
-
 const callMe = document.querySelector(".header__button");
 callMe.addEventListener("click" , () => alert('Позвони мне, позвони'))
 
+const btnClear = document.getElementById("btn-clear");// 
+btnClear.addEventListener("click" , () => {
+    for (let r = 1; r < 7; r++) {
+        document.getElementById(`select-${r}`)
+        .options[0].selected = 'selected'        
+    }
+    
+})
 
 // const btnTab = document.querySelectorAll('.filterContent__tabs-btn');
 // for (let i = 0; i < btnTab.length; i++) {
